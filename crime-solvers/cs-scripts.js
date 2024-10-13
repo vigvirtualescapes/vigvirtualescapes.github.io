98c8953951537cf3c5a6325fba886ffe65224243
Zach Vig <97557591+zacharyvig@users.noreply.github.com>
2024-10-13 18:58:11 -0400

    matchmaker scenario

---
+++ crime-solvers/cs-scripts.js
@@ -0,0 +1,49 @@
+// Toggle Theme
+function toggleTheme(sheet, name1, name2) {
+    var themeSwitch = document.getElementById('theme-switch');
+    var themeSheet = document.getElementById('secondary-theme-sheet');
+    var themeText = document.getElementById('theme-text');
+    var cover = document.getElementById('cover');
+
+    if (themeSwitch.checked == true) {
+        // cover.classList.remove('cover-open');
+        // cover.classList.add('cover-close');
+        // setTimeout(function() {
+            themeText.textContent = name2;
+            themeSheet.href = sheet; 
+        //     cover.classList.remove('cover-close');
+        //     cover.classList.add('cover-open');
+        // }, 1500);
+    } else {
+        // cover.classList.remove('cover-open');
+        // cover.classList.add('cover-close');
+        // setTimeout(function() {
+            themeText.textContent = name1;
+            themeSheet.href = null; 
+        //     cover.classList.remove('cover-close');
+        //     cover.classList.add('cover-open');
+        // }, 1500);
+    }
+
+}
+
+// Loader
+// var timeOut;
+
+// function loadFunc() {
+//     timeOut = setTimeout(revealBody, 5000);
+// }
+
+// function revealBody() {
+//     document.getElementById("loading-page").style.display = "none";
+//     document.getElementById("main-wrapper").style.display = "block";
+// }
+
+// Open window
+function closeWindow (buttonID, windowID) {
+    var closeButton = document.getElementById(buttonID);
+    var fullscreenWindow = document.getElementById(windowID);
+
+    fullscreenWindow.style.display = "none";
+}
+
